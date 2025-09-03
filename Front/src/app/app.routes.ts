db226490be449cdfc663d2088340a2e12b032ccc
ExequielCabrera <exequielcabrerafullstack@gmail.com>
2025-09-03 13:33:51 -0300

    creacion componente landingpage, home y correcciones componente inicio

--- Front/src/app/app.routes.ts
+++ Front/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
-import { CategoriasComponent } from './componentes/categorias/categorias.component';
 import { LandingComponent } from './landing/landing.component'; 
+import { HomeComponent } from './componentes/home/home.component';
+import { InicioComponent } from './componentes/inicio/inicio.component';
 
 export const routes: Routes = [
-  { path: '', component: LandingComponent },          
-  { path: 'categorias', component: CategoriasComponent }, 
+  { path: '', component: LandingComponent },   // página inicial
+  { 
+    path: 'home', 
+    component: HomeComponent, 
+    children:[
+      { path: '', redirectTo: 'inicio', pathMatch: 'full' }, // redirige al inicio
+      { path: 'inicio', component: InicioComponent }
+    ] 
+  }, 
   { path: '**', redirectTo: '', pathMatch: 'full' } 
 ];
--- Front/src/main.ts
+++ Front/src/main.ts
@@ -3,13 +3,13 @@ import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { routes } from './app/app.routes';
+import { routes } from './app/app.routes'; // 👈 importamos las rutas reales
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(
       HttpClientModule,
-      RouterModule.forRoot(routes)
+      RouterModule.forRoot(routes) // 👈 usamos estas rutas
     )
   ]
 });
