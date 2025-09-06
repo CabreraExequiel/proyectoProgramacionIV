82e0befee66370695f73b68a335218dce20b41f6
ExequielCabrera <exequielcabrerafullstack@gmail.com>
2025-09-05 21:17:51 -0300

    proyecto actualizado

--- Back/routes/api.php
+++ Back/routes/api.php
@@ -2,15 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CategoriaController;
-use App\Http\Controllers\CanchaController;
 use App\Http\Controllers\AuthController;  //Ultimo agregado
 use App\Http\Controllers\ReservaController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CanchaController2;
 
 
-Route::apiResource('canchas', CanchaController::class);
+
 Route::apiResource('reservas', ReservaController::class);
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']); //Ultimo agregado
